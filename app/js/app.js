68c4cd6339140c4df164d674ba29fd22776668d9
Anastasiia <nsydorchenko@gmail.com>
2021-05-26 15:45:05 +0300

    100vh mobile login page fix

--- app/js/app.js
+++ app/js/app.js
@@ -1,20 +1,8 @@
-// // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
-
-// // Import vendor jQuery plugin example (not module)
-// require('~/app/libs/mmenu/dist/mmenu.js')
-
 // 100VH MOBILE FIX
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 window.addEventListener('resize', () => {
-   let vh = window.innerHeight * 0.01;
-   document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-   if (window.innerWidth < 1025) {
-      swiperPeople.mousewheel.disable();
-   }
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
